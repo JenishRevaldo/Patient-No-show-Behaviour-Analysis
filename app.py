00d0978a5154a78bfe99acae409f8e069f188858
Jenish Revaldo <129158780+JenishRevaldo@users.noreply.github.com>
2024-01-22 16:27:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,43 +20,40 @@ def main():
     patient_id_options = original_df['PatientId'].unique()
     patient_id = st.selectbox("Patient ID", patient_id_options)
 
-    gender = st.selectbox("Gender", ["Male", "Female"])
     scheduled_day = st.date_input("Scheduled Day")
     appointment_day = st.date_input("Appointment Day")
-    age = st.number_input("Age", value=0)
+
 
     neighbourhood_options = original_df['Neighbourhood'].unique()
     neighbourhood = st.selectbox("Neighbourhood", neighbourhood_options)
 
-    scholarship = st.selectbox("Scholarship", ["No", "Yes"])
-    hipertension = st.selectbox("Hipertension", ["No", "Yes"])
-    diabetes = st.selectbox("Diabetes", ["No", "Yes"])
-    alcoholism = st.selectbox("Alcoholism", ["No", "Yes"])
-    handicap = st.selectbox("Handicap", ["No", "Yes"])
     sms_received = st.selectbox("SMS Received", ["No", "Yes"])
 
     # Validate PatientId against the original dataset
     if st.button("Submit"):
+        
+        patient_data = original_df[original_df['PatientId'] == patient_id].iloc[0]
+
         # Create a dictionary with the form data
         form_data = {
-            "PatientId": patient_id,
-            "Gender": gender,
+            "PatientId": patient_data['PatientId'],
+            "Gender": patient_data['Gender'],
             "ScheduledDay": scheduled_day,
             "AppointmentDay": appointment_day,
-            "Age": age,
+            "Age": patient_data['Age'],
             "Neighbourhood": neighbourhood,
-            "Scholarship": 1 if scholarship == "Yes" else 0,
-            "Hipertension": 1 if hipertension == "Yes" else 0,
-            "Diabetes": 1 if diabetes == "Yes" else 0,
-            "Alcoholism": 1 if alcoholism == "Yes" else 0,
-            "Handcap": 1 if handicap == "Yes" else 0,
-            "SMS_received": 1 if sms_received == "Yes" else 0,
-        }
+            "Scholarship": patient_data['Scholarship'],
+            "Hipertension": patient_data['Hipertension'],
+            "Diabetes": patient_data['Diabetes'],
+            "Alcoholism": patient_data['Alcoholism'],
+            "Handcap": patient_data['Handcap'],
+                "SMS_received": 1 if sms_received == "Yes" else 0,
+            }
 
         # Convert the form data to a DataFrame for further processing
 
         df = pd.DataFrame([form_data])
-        df['Gender'] = df['Gender'].map({'Male' : 1, 'Female' : 0})
+        df['Gender'] = df['Gender'].map({'M' : 1, 'F' : 0})
        
         df['ScheduledDay'] = pd.to_datetime(df['ScheduledDay'])
         df['AppointmentDay'] = pd.to_datetime(df['AppointmentDay'])
@@ -79,16 +76,28 @@ def main():
         X = df[['Gender','Age','Scholarship','Hipertension','Diabetes','Alcoholism','Handcap',
                 'SMS_received','Day_of_Reg','Day_of_App','Diff_in_Date','Diff_in_Month',
                 'Popularity_Score','Preferred_Day']]
-        
+
         prediction = model.predict(X)
         
         if prediction == 1:
-            prediction = 'The Person may not show up for the appoinment'
+            prediction = 'This Person may not show up for the appoinment'
         else:
-            prediction = 'The Person may show up for the appoinment'
+            prediction = 'This Person may show up for the appoinment'
         
-        st.write("Prediction :")
-        st.write(prediction)
+        st.markdown(f"**Patient Details:**\n\n"
+                        f"- Patient ID: {form_data['PatientId']}\n"
+                        f"- Scheduled Day: {form_data['ScheduledDay']}\n"
+                        f"- Appointment Day: {form_data['AppointmentDay']}\n"
+                        f"- Gender: {form_data['Gender']}\n"
+                        f"- Age: {form_data['Age']}\n"
+                        f"- Neighbourhood: {form_data['Neighbourhood']}\n"
+                        f"- Scholarship: {'Yes' if form_data['Scholarship'] == 1 else 'No'}\n"
+                        f"- Hipertension: {'Yes' if form_data['Hipertension'] == 1 else 'No'}\n"
+                        f"- Diabetes: {'Yes' if form_data['Diabetes'] == 1 else 'No'}\n"
+                        f"- Alcoholism: {'Yes' if form_data['Alcoholism'] == 1 else 'No'}\n"
+                        f"- Handicap: {'Yes' if form_data['Handcap'] == 1 else 'No'}\n"
+                        f"- SMS Received: {'Yes' if form_data['SMS_received'] == 1 else 'No'}\n\n"
+                        f"**Prediction:** {prediction}")
 
 if __name__ == "__main__":
     main()
